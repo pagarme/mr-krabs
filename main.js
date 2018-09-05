19d403c8671fb15ca54c6b9b187c8254ac2190ca
Luciano Franca <lucianopf@outlook.com>
2018-09-05 18:37:01 -0300

    fix: uses real application path
    Instead of using the module path we're now using the apps relative path

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+const path = require('path')
 const mrKrabs = require('./lib')
-const configFile = require('./krabs.json')
+const configFile = require(path.dirname(require.main.filename) + '/krabs.json')
 
 module.exports = Object.keys(mrKrabs)
   .reduce((result, attributeKey) => {
