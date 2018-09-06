c1ad439fb2568ede8f02a4996ee747fb5cef8549
Luciano Franca <lucianopf@outlook.com>
2018-09-06 18:09:23 -0300

    entrypoing: change the entrypoint
    changed the entrypoint from main.js to  lib/index.js in order to enhance intelisense from code editors and also enhanced the config file injection

--- main.js
+++
@@ -1,8 +0,0 @@
-const mrKrabs = require('./lib')
-const configFile = require('./krabs.json')
-
-module.exports = Object.keys(mrKrabs)
-  .reduce((result, attributeKey) => {
-    result[attributeKey] = mrKrabs[attributeKey].bind(null, configFile)
-    return result
-  }, {})
